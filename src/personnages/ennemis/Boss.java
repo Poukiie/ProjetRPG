e0becae4e4c114528b797bb055fe52a5153ecd0b
Pebble <limrelena08@gmail.com>
2023-05-21 21:21:35 +0200

    fix implement interface in Boss

--- src/personnages/ennemis/Boss.java
+++ src/personnages/ennemis/Boss.java
@@ -1,12 +1,14 @@
 package personnages.ennemis;
 
-public class Boss extends Ennemi {
+import personnages.CapaciteSpeciale;
+
+public class Boss extends Ennemi implements CapaciteSpeciale {
     public Boss(String nom) {
         super(nom, 400, 400, 100, 80, 75, 60, 50);
     }
 
     @Override
     public void utiliserCapacite() {
-
+        
     }
 }
--- src/personnages/ennemis/Ennemi.java
+++ src/personnages/ennemis/Ennemi.java
@@ -1,13 +1,9 @@
 package personnages.ennemis;
 
 import personnages.Personnage;
-import personnages.CapaciteSpeciale;
 
-public abstract class Ennemi extends Personnage implements CapaciteSpeciale {
+public abstract class Ennemi extends Personnage {
     public Ennemi(String nom, int pv, int pvMax, int atq, int def, int precision, int esquive, int energie) {
         super(nom, pv, pvMax, atq, def, precision, esquive, energie);
     }
-
-    @Override
-    public abstract void utiliserCapacite();
 }
