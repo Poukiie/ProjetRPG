3e16c77c07f62765fa32ae49a52df7ba68e32650
yusa-ai <64932650+yusa-ai@users.noreply.github.com>
2023-05-21 11:25:11 +0200

    Handle precision and dodge

--- src/personnages/Personnage.java
+++ src/personnages/Personnage.java
@@ -1,12 +1,15 @@
 package personnages;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Personnage {
     private String nom;
     private int PV;
     private int ATK;
     private int DEF; // 5 de DEF, l’ennemi tape 12, je me prends 7 de DGT, différence
+    private int precision;
+    private int esquive;
     private int energie; // pour utiliser des capacités
     private static int counter = 0;
     private static final ArrayList<Personnage> personnages = new ArrayList<>();
@@ -16,6 +19,8 @@ public class Personnage {
         this.PV = pv;
         this.ATK = atq;
         this.DEF = def;
+        this.precision = 70;
+        this.esquive = 30;
         this.energie = energie;
         Personnage.counter += 1;
     }
@@ -34,6 +39,14 @@ public class Personnage {
 
     public int getDEF() {
         return DEF;
+    }
+
+    public int getPrecision() {
+        return precision;
+    }
+
+    public int getEsquive() {
+        return esquive;
     }
 
     public int getEnergie() {
@@ -56,21 +69,31 @@ public class Personnage {
         personnages.add(new Personnage(nom, pv, atq, def, energie));
     }
 
+    private boolean roll(int stat) {
+        return new Random().nextInt(100) + 1 <= stat;
+    }
+
     public void attaquer(Personnage p) {
         int valeurAttaque = this.ATK - p.getDEF();
-
-        if (valeurAttaque <= 0) {
-            System.out.println(this.nom + " n'inflige aucun dégât à " + p.getNom()
-                    + " (" + (p.getPV()) + "PV)");
+        if (valeurAttaque > 0) {
+            // Check précision
+            boolean attaquePrecise = this.roll(this.precision);
+            if (attaquePrecise) {
+                // Check esquive adversaire
+                boolean attaqueEsquivee = p.roll(p.getEsquive());
+                if (!attaqueEsquivee) {
+                    int diff = p.getPV() - valeurAttaque;
+                    int newPV = Math.max(diff, 0);
+                    p.setPV(newPV);
+                    System.out.println(this.nom + " inflige " + valeurAttaque + " dégâts à " + p.getNom()
+                            + " (" + (newPV) + "PV)");
+                    return;
+                }
+            }
         }
 
-        else {
-            int diff = p.getPV() - valeurAttaque;
-            int newPV = Math.max(diff, 0);
-            p.setPV(newPV);
-            System.out.println(this.nom + " inflige " + valeurAttaque + " dégâts à " + p.getNom()
-                    + " (" + (newPV) + "PV)");
-        }
+        System.out.println(this.nom + " n'inflige aucun dégât à " + p.getNom()
+                + " (" + (p.getPV()) + "PV)");
     }
 
     public void utiliserCapacite(Personnage p) {
