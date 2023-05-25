509136791e989a605c39dfc4151de4d0005425a3
yusa-ai <64932650+yusa-ai@users.noreply.github.com>
2023-05-25 23:27:40 +0200

    Handle changing salle on completion

--- src/Partie.java
+++ src/Partie.java
@@ -23,63 +23,65 @@ public class Partie {
     }
 
     public void lancerDonjon() {
-        // Tant que le donjon n'est pas terminé
-        System.out.println("-------- Salle " + salleActuelle + 1 + " --------");
-        while (!salles.get(salleActuelle).isCompleted()) {
-            // Tant que l'équipe n'est pas morte
-            while (allies.size() > 0) {
-                // Tant que l'équipe n'a pas tué tous les ennemis
-                while (ennemis.size() > 0) {
-                    // L'équipe attaque
-                    System.out.println("-------- Tour de l'équipe --------");
-                    for (Personnage personnage : allies) {
-                        // Vérifier que le personnage n'est pas mort
-                        if (personnage.getPV() > 0) {
-                            System.out.println("Que doit faire " + personnage.getNom() + " ?\n" +
-                                    "PV : " + personnage.getPV() + "\n" +
-                                    "Energie : " + personnage.getEnergie() + "\n"
-                            );
-                            System.out.println("1. Attaquer un ennemi");
-                            System.out.println("2. Utiliser sa capacité spéciale");
-                            System.out.println("3. Utiliser un objet");
-                            System.out.println();
+        while (salleActuelle != 3 && !salles.get(salleActuelle).isCompleted()) {
+            System.out.println("-------- Salle " + salleActuelle + 1 + " --------");
+            while (!salles.get(salleActuelle).isCompleted()) {
+                // Tant que l'équipe n'est pas morte
+                while (allies.size() > 0) {
+                    // Tant que l'équipe n'a pas tué tous les ennemis
+                    while (ennemis.size() > 0) {
+                        // L'équipe attaque
+                        System.out.println("-------- Tour de l'équipe --------");
+                        for (Personnage personnage : allies) {
+                            // Vérifier que le personnage n'est pas mort
+                            if (personnage.getPV() > 0) {
+                                System.out.println("Que doit faire " + personnage.getNom() + " ?\n" +
+                                        "PV : " + personnage.getPV() + "\n" +
+                                        "Energie : " + personnage.getEnergie() + "\n"
+                                );
+                                System.out.println("1. Attaquer un ennemi");
+                                System.out.println("2. Utiliser sa capacité spéciale");
+                                System.out.println("3. Utiliser un objet");
+                                System.out.println();
 
-                            int choice = Integer.parseInt(sc.nextLine());
-                            switch (choice) {
-                                case 1:
-                                    System.out.println("Quel ennemi souhaitez-vous attaquer ?");
-                                    System.out.println(ennemis);
+                                int choice = Integer.parseInt(sc.nextLine());
+                                switch (choice) {
+                                    case 1:
+                                        System.out.println("Quel ennemi souhaitez-vous attaquer ?");
+                                        System.out.println(ennemis);
 
-                                    // TODO vérifier saisie
-                                    int ennemiCible = Integer.parseInt(sc.nextLine()) -1;
-                                    personnage.attaquer(ennemis.get(ennemiCible));
+                                        // TODO vérifier saisie
+                                        int ennemiCible = Integer.parseInt(sc.nextLine()) -1;
+                                        personnage.attaquer(ennemis.get(ennemiCible));
 
-                                    // Si l'ennemi est mort, le supprimer de la liste
-                                    if (ennemis.get(ennemiCible).getPV() <= 0) {
-                                        ennemis.remove(ennemiCible);
-                                    }
-                                    break;
+                                        // Si l'ennemi est mort, le supprimer de la liste
+                                        if (ennemis.get(ennemiCible).getPV() <= 0) {
+                                            ennemis.remove(ennemiCible);
+                                        }
+                                        break;
 
-                                case 2:
+                                    case 2:
 
+                                }
                             }
                         }
-                    }
 
-                    // Les ennemis attaquent
-                    for (Ennemi ennemi : ennemis) {
-                        // Attaquer un personnage aléatoire
-                        int personnageCible = new Random().nextInt(allies.size());
-                        ennemi.attaquer(allies.get(personnageCible));
-                        // Si le personnage est mort, le supprimer de la liste
-                        if (allies.get(personnageCible).getPV() <= 0) {
-                            personnagesMorts.add(allies.get(personnageCible));
-                            allies.remove(personnageCible);
+                        // Les ennemis attaquent
+                        for (Ennemi ennemi : ennemis) {
+                            // Attaquer un personnage aléatoire
+                            int personnageCible = new Random().nextInt(allies.size());
+                            ennemi.attaquer(allies.get(personnageCible));
+                            // Si le personnage est mort, le supprimer de la liste
+                            if (allies.get(personnageCible).getPV() <= 0) {
+                                personnagesMorts.add(allies.get(personnageCible));
+                                allies.remove(personnageCible);
+                            }
                         }
-                    }
 
+                    }
                 }
             }
+            salleActuelle++;
         }
     }
 }
